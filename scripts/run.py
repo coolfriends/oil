2931c941ba21cbe37fc261c7c4307c6291b8a1d5
kbougy <kyriayxvanderpoel@gmail.com>
2017-11-07 22:37:58 -0600

    Add switch for csv to run script

--- scripts/run.py
+++ scripts/run.py
@@ -1,6 +1,9 @@
 import sys
 import os
 import json
+import csv
+import argparse
+
 
 try:
     from oil import Oil
@@ -9,5 +12,60 @@ except ImportError: # If module not installed with pip
     sys.path.insert(0, oil_path)
     from oil import Oil
 
-oil = Oil()
-print(json.dumps(oil.scan(), indent=2))
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Run a basic oil scan."
+    )
+    choices = ['json', 'csv']
+    parser.add_argument('--output',
+                        help='set output as json or csv',
+                        choices=choices,
+                        default='json')
+    return parser.parse_args()
+
+def generate_flattened_dict(scan_data):
+    for provider, services in scan_data.items():
+        for service, calls in services.items():
+            for call, results in calls.items():
+                for result in results:
+                    row = {
+                        'provider': provider,
+                        'service': service,
+                        'title': call,
+                        'region': result['region'],
+                        'resource': result['resource'],
+                        'severity': result['severity'],
+                        'message': result['message']
+                    }
+                    yield row
+
+def print_as_csv(scan_data):
+    fields = (
+        'provider',
+        'service',
+        'title',
+        'region',
+        'resource',
+        'severity',
+        'message'
+    )
+
+    writer = csv.DictWriter(sys.stdout, fieldnames=fields)
+    writer.writeheader()
+    for row in generate_flattened_dict(scan_data):
+        writer.writerow(row)
+
+def main():
+    args = parse_args()
+
+    oil = Oil()
+    data = oil.scan()
+    if args.output == 'csv':
+        print_as_csv(data)
+    elif args.output == 'json':
+        print(json.dumps(data, indent=2))
+    else:
+        print('Unsupported output format.')
+
+if __name__ == "__main__":
+    main()
