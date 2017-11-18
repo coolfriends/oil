c81432d4e6c400b5ac0c07d6d7bbd539c92d4334
Kyri Vanderpoel <kbougy@users.noreply.github.com>
2017-11-18 00:44:00 -0600

    Issue 70 (#72)
    * Read version number from __init__.py to avoid imports
    
    * Add note to version number

--- oil/__init__.py
+++ oil/__init__.py
@@ -1,2 +1,5 @@
+__version__ = '0.1'
+# NOTE: Don't bump the version number until we are deployed to PyPI
+
+
 from oil.oil import Oil
-from oil.utils import version
--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
+import re
 from setuptools import setup, find_packages
 
-version = __import__('oil').version()
+# get the version as a string, assuming it's at the top of oil/__init__.py
+version_reg = re.compile('\d.\d')
+with open('oil/__init__.py') as f:
+    match = version_reg.search(f.readline())
+    _version = match[0]
 
 # For future package excludes, such as 'oil.config'
 EXCLUDES = []
 
 setup(
     name="oil",
-    version=version,
+    version=_version,
     url="https://github.com/coolfriends/oil",
     author="Cabal",
     description='An extensible framework for auditing cloud resources.',
