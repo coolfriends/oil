3b51396436a0d3675d1db7805a283728c484a952
kbougy <kyriayxvanderpoel@gmail.com>
2018-01-07 11:00:32 -0600

    Implemented and tested cloudtrail barrel

--- oil/barrels/aws/__init__.py
+++ oil/barrels/aws/__init__.py
@@ -1,4 +1,5 @@
 from oil.barrels.aws.autoscaling import AutoScalingBarrel
+from oil.barrels.aws.cloudtrail import CloudTrailBarrel
 from oil.barrels.aws.cloudfront import CloudFrontBarrel
 from oil.barrels.aws.iam import IAMBarrel
 from oil.barrels.aws.ec2 import EC2Barrel
@@ -7,6 +8,7 @@ from oil.barrels.aws.rds import RDSBarrel
 core_barrels = [
     AutoScalingBarrel,
     CloudFrontBarrel,
+    CloudTrailBarrel,
     IAMBarrel,
     EC2Barrel,
     RDSBarrel,
